077b762ce8d58dbd1dc6b12dcaed94f3b2e9495f
Stina Persson <stpe1901@student.miun.se>
2025-03-04 09:29:59 +0100

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace projektdotnet.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
