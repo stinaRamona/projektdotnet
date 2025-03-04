d3486727ca9df9359facc0b587ff18a84105dd9b
Stina Persson <stpe1901@student.miun.se>
2025-03-04 12:07:36 +0100

    API skapat som hämtar in produkter och returnerar datan i json

---
+++ Controllers/ProductApiController.cs
@@ -0,0 +1,31 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using projektdotnet.Data;
+
+namespace MyApp.Namespace
+{
+    [Route("api/products")]
+    [ApiController]
+    public class ProductApiController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context; 
+
+        public ProductApiController(ApplicationDbContext context) 
+        {
+            _context = context; 
+        }
+
+        //GET anrop för att kunna hämta data från db som json
+        [HttpGet]
+        public async Task<IActionResult> GetProducts() 
+        {
+            if(_context.Products == null)
+            {
+                return NotFound(); 
+            }
+
+            return Ok(await _context.Products.ToListAsync());
+        }
+    }
+}
