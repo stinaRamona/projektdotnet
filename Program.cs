0768af35b1b361012db66f553480c46733bc4973
Stina Persson <stpe1901@student.miun.se>
2025-03-11 14:08:48 +0100

    Uppdaterat så CORS är ok

--- Program.cs
+++ Program.cs
@@ -14,7 +14,9 @@ builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.Requ
     .AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddControllersWithViews();
 
+
 var app = builder.Build();
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -27,6 +29,13 @@ else
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
+//middleware
+app.UseCors(builder => builder 
+    .AllowAnyOrigin()
+    .AllowAnyMethod()
+    .AllowAnyHeader()
+); 
 
 app.UseHttpsRedirection();
 app.UseRouting();
